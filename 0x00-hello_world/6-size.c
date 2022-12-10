84ac31e53d8e0cd2e9b6c62d91f7671e73a098b1
Your Name <gediondemise@gmail.com>
2022-12-10 14:34:52 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,9 +2,8 @@
 
 /**
 *
-*
-*main - prints the size of various types on the computer it is compiled and run 
-*Return: 0 if exited properly, non-zero otherwise
+* main - prints the size of various types on the computer it is compiled and run 
+* Return: 0 if exited properly, non-zero otherwise
 */
 int main(void)
 {
