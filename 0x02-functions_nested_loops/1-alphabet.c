b7e6167806a035bd5ec3bbf35a6d7e43d3cfe08e
Your Name <gediondemise@gmail.com>
2022-12-14 14:38:51 -0800

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a new line
+ */
+void print_alphabet(void)
+{
+
+char ge;
+ge = 'a';
+
+while (ge <= 'z')
+{
+
+_putchar(ge);
+ge++;
+}
+_putchar('\n');
+
+}
