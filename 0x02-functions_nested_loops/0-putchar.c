652d189a3e8015430b3c370ef973eab388f57e13
Your Name <gediondemise@gmail.com>
2022-12-14 13:52:39 -0800

    new nessted loops by gedion alx

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include<stdio.h.>
+
+/**
+ *main - prints _putchar, followed by a new line
+ *Return: 0
+ *
+ */
+int main(void)
+{
+char *ge = "_putchar";
+while (ge)
+{
+_putchar(*ge)
+ge++
+}
+_putchar('/n');
+return (0);
+
+}
