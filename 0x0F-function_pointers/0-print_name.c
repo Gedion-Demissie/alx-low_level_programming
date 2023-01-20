f8b2c48f4abecafdb75b8a8426d20bbbbfc4854c
Your Name <gediondemise@gmail.com>
2023-01-20 12:02:08 -0800

    #include <stddef.h>
    '

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,15 +1,18 @@
 #include "function_pointers.h"
+#include <stdio.h>
 
 /**
- * aprint_name - prints a name
- *  @name: the string name
- *  @f: the printing function pointer
- *
- *   Return: Always 0
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
  */
-void print_name(char *name, void (*f)(char *)
+
+void print_name(char *name, void (*f)(char *))
 {
-if (name && f)
+if (name == NULL || f == NULL)
+return;
 
 f(name);
+
 }
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,6 +3,9 @@
 
 
 #include <stdlib.h>
+#include <stddef.h>
+
+
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
