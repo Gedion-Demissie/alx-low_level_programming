6a9e6d817d05eb922928362af277f361d81b5693
Your Name <gediondemise@gmail.com>
2023-01-31 13:04:17 -0800

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,18 +8,14 @@
  */
 
 
-
-
 size_t print_listint(const listint_t *h)
 {
-if (h == NULL)
-printf("no data to be prited");
-struct listint_s *ptr = NULL;
-ptr = h;
-while (ptr != 0)
+size_t num = 0;
+while (h)
 {
-printf("%d\n", ptr->n);
-ptr = ptr->next;
+printf("%d\n", h->n);
+num++;
+h = h->next;
 }
-
+return (num);
 }
